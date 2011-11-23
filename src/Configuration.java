1329a9aa4353ddb5524eac0436f2101fd5d6b8e4
Ulrich Tewes (Work) <ulrich.tewes@googlemail.com>
2011-11-23 17:27:58 +0100

    added Comment about purpose

--- src/Configuration.java
+++ src/Configuration.java
@@ -3,7 +3,7 @@
  * User: ulrich.tewes
  * Date: 11/22/11
  * Time: 11:25 AM
- * To change this template use File | Settings | File Templates.
+ * This is a Dummy Config (without a config file, which is coming soon)
  */
 public class Configuration {
 	private boolean fDrawTiles;
