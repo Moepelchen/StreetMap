8de9b45e137f94895213e422bc154fbdbcb12076
ulrich tewes <ulrichtewes@ulrichs-MacBook-Air.local>
2012-01-23 07:29:44 +0100

    Scientific library for spline interpolation

---
+++ src/streetmap/BendTrajectory.java
@@ -0,0 +1,19 @@
+package streetmap;
+
+import streetmap.Interfaces.ITrajectory;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ulrichtewes
+ * Date: 23.01.12
+ * Time: 07:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class BendTrajectory implements ITrajectory{
+    @Override
+    public Point2D calculatePosition(Point2D pos, double speed) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
